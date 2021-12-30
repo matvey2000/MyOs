ab4d1a52098911da85a8af0553c502f7cb9d70c8
matvey2000 <35378456+matvey2000@users.noreply.github.com>
2021-12-30 15:30:47 +0300

    Add files via upload

--- code/software.asm
+++ code/software.asm
@@ -14,4 +14,35 @@ headerend:
 	
 	times 0x12000-$+begin db 0
 start:
-	
+	mov bx, mess
+	call print
+	jmp $
+print:
+	;bx = offset message
+	push ax
+	push bx
+	push cx
+	push dx
+	lpsprint:
+		push bx
+		mov ah, 0xe
+		mov al, byte [bx]
+		
+		cmp al, 0
+		je endprint
+		
+		xor bh, bh
+		int 0x10
+		pop bx
+		
+		add bx, 1
+		
+		jmp lpsprint
+	endprint:
+		pop bx
+		pop dx
+		pop cx
+		pop bx
+		pop ax
+		ret
+mess: db "this is test",0
