55af848b9d27fd2870a07e71b2ff25979d53f1d3
matvey2000 <35378456+matvey2000@users.noreply.github.com>
2021-12-30 15:23:34 +0300

    Add files via upload

--- code/software.asm
+++ code/software.asm
@@ -1,7 +1,8 @@
 use16
 org 0x800
 
-times 0x4800 db 0
+begin:
+	times 0x4800 db 0
 header:
 	db "file", 0
 	times 0x64-$+header db 0
@@ -11,5 +12,7 @@ headerend:
 	db "end", 0
 	times 0x64-$+headerend db 0
 	dw 0x0001
+	
+	times 0x12000-$+begin db 0
 start:
 	
