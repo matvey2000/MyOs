ecc099fe310db306572474526daeb85f773b7983
matvey2000 <35378456+matvey2000@users.noreply.github.com>
2021-12-31 15:08:27 +0300

    Add files via upload

---
+++ code/header.asm
@@ -0,0 +1,11 @@
+use16
+begin:
+	times 0x4800 db 0
+header:
+	db "file", 0
+	times 0x64-$+header db 0
+	dw 0x0000
+	
+headerend:
+	times 0x64-$+headerend db 0
+	dw 0x0001
--- code/software.asm
+++ code/software.asm
@@ -1,21 +1,9 @@
 use16
 org 0x4C00
-begin:
-	times 0x4800 db 0
-header:
-	db "file", 0
-	times 0x64-$+header db 0
-	dw 0x0000
-	
-headerend:
-	times 0x64-$+headerend db 0
-	dw 0x0001
-	
-	times 0x12000-$+begin db 0
 start:
 	mov ch, 0x1
 	mov bx, 16
-	mov ax, 0x2A
+	mov ax, mess
 	int 0x22
 	
 	int 0x20
